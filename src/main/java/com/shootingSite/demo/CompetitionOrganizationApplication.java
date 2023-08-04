3a508c984cb46c4d32d8da99cae060000f255961
Weronika Graczyk <wercia01@onet.pl>
2023-08-04 11:48:54 +0200

    initial commit

---
+++ src/main/java/com/shootingSite/demo/CompetitionOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.shootingSite.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionOrganizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shootingSite/demo/CompetitionOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shootingSite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
