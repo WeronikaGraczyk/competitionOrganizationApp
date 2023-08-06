a0ebd859e6d6c9a81fa35d6ed2a7048dde18cae9
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 12:59:58 +0200

    create category controller

---
+++ src/main/java/com/shootingSite/demo/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.shootingSite.demo.controller;
+
+import com.shootingSite.demo.entity.Category;
+import com.shootingSite.demo.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        return categoryService.getCategoryById(id);
+    }
+
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
+}
