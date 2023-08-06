0021dbc34abb2d6ad0cd3ef7773c84dc0f09d729
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 13:00:37 +0200

    create productImage service

---
+++ src/main/java/com/shootingSite/demo/service/ProductImageService.java
@@ -0,0 +1,41 @@
+package com.shootingSite.demo.service;
+
+import com.shootingSite.demo.entity.Product;
+import com.shootingSite.demo.entity.ProductImage;
+import com.shootingSite.demo.exception.ResourceNotFoundException;
+import com.shootingSite.demo.repository.ProductImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class ProductImageService {
+    private final ProductImageRepository productImageRepository;
+    private final ProductService productService;
+    @Autowired
+    public ProductImageService(ProductImageRepository productImageRepository, ProductService productService) {
+        this.productImageRepository = productImageRepository;
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ProductImage saveProductImage(MultipartFile image,Long productId) throws IOException {
+
+        Product product = productService.getProductById(productId);
+
+        if (product == null) {
+            throw new ResourceNotFoundException("Product doesn't exist!");
+        }
+
+        ProductImage productImage = ProductImage.builder()
+                .image(image.getBytes())
+                .product(product)
+                .build();
+
+        return productImageRepository.save(productImage);
+    }
+}
