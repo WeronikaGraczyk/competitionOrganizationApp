e2509c5cb9ed2856ac8d7a332278546654f57833
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 09:22:03 +0200

    create ProductImage Entity

---
+++ src/main/java/com/shootingSite/demo/entity/ProductImage.java
@@ -0,0 +1,25 @@
+package com.shootingSite.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private byte[] image;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
