2f30b7728805fd44f8c039d5595cad97fad2ea90
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 10:25:55 +0200

    change ProductImage Repository entity

---
+++ src/main/java/com/shootingSite/demo/repository/ProductImageRepository.java
@@ -0,0 +1,9 @@
+package com.shootingSite.demo.repository;
+
+import com.shootingSite.demo.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+}
