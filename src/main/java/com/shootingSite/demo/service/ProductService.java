950027d6048a642f2c6f91790c81c58926836496
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 10:25:33 +0200

    change Product Service entity

---
+++ src/main/java/com/shootingSite/demo/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.shootingSite.demo.service;
+
+import com.shootingSite.demo.entity.Product;
+import com.shootingSite.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
