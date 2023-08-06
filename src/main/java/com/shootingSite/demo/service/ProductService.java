5afb319f9df9b6e1f4688d9de3ba9230a466809e
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 12:59:34 +0200

    create product service

--- src/main/java/com/shootingSite/demo/service/ProductService.java
+++ src/main/java/com/shootingSite/demo/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.shootingSite.demo.service;
 
+import com.shootingSite.demo.entity.Category;
 import com.shootingSite.demo.entity.Product;
+import com.shootingSite.demo.exception.ResourceNotFoundException;
 import com.shootingSite.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,12 @@ import java.util.List;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryService categoryService;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryService categoryService) {
         this.productRepository = productRepository;
+        this.categoryService = categoryService;
     }
 
     public List<Product> getAllProducts() {
@@ -24,7 +28,12 @@ public class ProductService {
         return productRepository.findById(id).orElse(null);
     }
 
-    public Product saveProduct(Product product) {
+    public Product saveProduct(Product product, Long categoryId) {
+        Category category = categoryService.getCategoryById(categoryId);
+        if(category==null){
+            throw new ResourceNotFoundException("Category doesn't exist!");
+        }
+        product.setCategory(category);
         return productRepository.save(product);
     }
 
