fbd937c6a3dcaf9681d5dd929a65b28179053997
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 09:21:38 +0200

    create Product

---
+++ src/main/java/com/shootingSite/demo/entity/Product.java
@@ -0,0 +1,26 @@
+package com.shootingSite.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    private String category;
+}
