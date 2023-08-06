f9923c6b5be37f3e51f9f13e2e420986fc722e75
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 10:24:37 +0200

    create Category entity

---
+++ src/main/java/com/shootingSite/demo/entity/Category.java
@@ -0,0 +1,17 @@
+package com.shootingSite.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
