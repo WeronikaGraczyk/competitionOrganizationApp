3cdf929b41fd1c52a19b3acdc420ed4c74dee5ba
Weronika Graczyk <wercia01@onet.pl>
2023-08-06 13:00:23 +0200

    create category service

---
+++ src/main/java/com/shootingSite/demo/service/CategoryService.java
@@ -0,0 +1,31 @@
+package com.shootingSite.demo.service;
+
+import com.shootingSite.demo.entity.Category;
+import com.shootingSite.demo.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id){
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+    public Category saveCategory(Category category){
+        return categoryRepository.save(category);
+    }
+}
