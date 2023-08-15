3f7048cfb9ee35679385244a85b41c1956087881
Weronika Graczyk <wercia01@onet.pl>
2023-08-15 13:05:30 +0200

    create UserRepository

---
+++ src/main/java/com/shootingSite/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.shootingSite.demo.repository;
+
+import com.shootingSite.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
